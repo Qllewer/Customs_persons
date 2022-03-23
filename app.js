201f7bbd02b336253817646535964fafef2d0db0
Dmitriy Artemyev <dmitrie098@gmail.com>
2022-03-23 21:40:39 +0300

    Custom_persons

---
+++ app.js
@@ -0,0 +1,312 @@
+document.querySelector(".hair1-js").addEventListener('click', () => {
+    document.querySelector(".hair_2-js").hidden = true
+    document.querySelector(".hair_1-js").hidden = false
+})
+
+document.querySelector(".hair1-js").addEventListener('click', () => {
+    document.querySelector(".hair_3-js").hidden = true
+    document.querySelector(".hair_1-js").hidden = false
+})
+
+document.querySelector(".hair1-js").addEventListener('click', () => {
+    document.querySelector(".hair_4-js").hidden = true
+    document.querySelector(".hair_1-js").hidden = false
+})
+
+document.querySelector(".hair1-js").addEventListener('click', () => {
+    document.querySelector(".hair_5-js").hidden = true
+    document.querySelector(".hair_1-js").hidden = false
+})
+
+document.querySelector(".hair2-js").addEventListener('click', () => {
+    document.querySelector(".hair_1-js").hidden = true
+    document.querySelector(".hair_2-js").hidden = false
+})
+
+document.querySelector(".hair2-js").addEventListener('click', () => {
+    document.querySelector(".hair_3-js").hidden = true
+    document.querySelector(".hair_2-js").hidden = false
+})
+
+document.querySelector(".hair2-js").addEventListener('click', () => {
+    document.querySelector(".hair_4-js").hidden = true
+    document.querySelector(".hair_2-js").hidden = false
+})
+
+document.querySelector(".hair2-js").addEventListener('click', () => {
+    document.querySelector(".hair_5-js").hidden = true
+    document.querySelector(".hair_2-js").hidden = false
+})
+
+
+document.querySelector(".hair3-js").addEventListener('click', () => {
+    document.querySelector(".hair_2-js").hidden = true
+    document.querySelector(".hair_3-js").hidden = false
+})
+
+document.querySelector(".hair3-js").addEventListener('click', () => {
+    document.querySelector(".hair_1-js").hidden = true
+    document.querySelector(".hair_3-js").hidden = false
+})
+
+document.querySelector(".hair3-js").addEventListener('click', () => {
+    document.querySelector(".hair_4-js").hidden = true
+    document.querySelector(".hair_3-js").hidden = false
+})
+
+document.querySelector(".hair3-js").addEventListener('click', () => {
+    document.querySelector(".hair_5-js").hidden = true
+    document.querySelector(".hair_3-js").hidden = false
+})
+
+document.querySelector(".hair4-js").addEventListener('click', () => {
+    document.querySelector(".hair_1-js").hidden = true
+    document.querySelector(".hair_4-js").hidden = false
+})
+
+document.querySelector(".hair4-js").addEventListener('click', () => {
+    document.querySelector(".hair_2-js").hidden = true
+    document.querySelector(".hair_4-js").hidden = false
+})
+
+document.querySelector(".hair4-js").addEventListener('click', () => {
+    document.querySelector(".hair_3-js").hidden = true
+    document.querySelector(".hair_4-js").hidden = false
+})
+
+document.querySelector(".hair4-js").addEventListener('click', () => {
+    document.querySelector(".hair_5-js").hidden = true
+    document.querySelector(".hair_4-js").hidden = false
+})
+
+document.querySelector(".hair5-js").addEventListener('click', () => {
+    document.querySelector(".hair_1-js").hidden = true
+    document.querySelector(".hair_5-js").hidden = false
+})
+
+document.querySelector(".hair5-js").addEventListener('click', () => {
+    document.querySelector(".hair_2-js").hidden = true
+    document.querySelector(".hair_5-js").hidden = false
+})
+
+document.querySelector(".hair5-js").addEventListener('click', () => {
+    document.querySelector(".hair_3-js").hidden = true
+    document.querySelector(".hair_5-js").hidden = false
+})
+
+document.querySelector(".hair5-js").addEventListener('click', () => {
+    document.querySelector(".hair_4-js").hidden = true
+    document.querySelector(".hair_5-js").hidden = false
+})
+
+document.querySelector(".eyes1-js").addEventListener('click', () => {
+    document.querySelector(".eyes_2-js").hidden = true
+    document.querySelector(".eyes_1-js").hidden = false
+})
+
+document.querySelector(".eyes1-js").addEventListener('click', () => {
+    document.querySelector(".eyes_3-js").hidden = true
+    document.querySelector(".eyes_1-js").hidden = false
+})
+
+document.querySelector(".eyes1-js").addEventListener('click', () => {
+    document.querySelector(".eyes_4-js").hidden = true
+    document.querySelector(".eyes_1-js").hidden = false
+})
+
+document.querySelector(".eyes1-js").addEventListener('click', () => {
+    document.querySelector(".eyes_5-js").hidden = true
+    document.querySelector(".eyes_1-js").hidden = false
+})
+
+document.querySelector(".eyes2-js").addEventListener('click', () => {
+    document.querySelector(".eyes_1-js").hidden = true
+    document.querySelector(".eyes_2-js").hidden = false
+})
+
+document.querySelector(".eyes2-js").addEventListener('click', () => {
+    document.querySelector(".eyes_3-js").hidden = true
+    document.querySelector(".eyes_2-js").hidden = false
+})
+
+document.querySelector(".eyes2-js").addEventListener('click', () => {
+    document.querySelector(".eyes_4-js").hidden = true
+    document.querySelector(".eyes_2-js").hidden = false
+})
+
+document.querySelector(".eyes2-js").addEventListener('click', () => {
+    document.querySelector(".eyes_5-js").hidden = true
+    document.querySelector(".eyes_2-js").hidden = false
+})
+
+document.querySelector(".eyes3-js").addEventListener('click', () => {
+    document.querySelector(".eyes_1-js").hidden = true
+    document.querySelector(".eyes_3-js").hidden = false
+})
+
+document.querySelector(".eyes3-js").addEventListener('click', () => {
+    document.querySelector(".eyes_2-js").hidden = true
+    document.querySelector(".eyes_3-js").hidden = false
+})
+
+document.querySelector(".eyes3-js").addEventListener('click', () => {
+    document.querySelector(".eyes_4-js").hidden = true
+    document.querySelector(".eyes_3-js").hidden = false
+})
+
+document.querySelector(".eyes3-js").addEventListener('click', () => {
+    document.querySelector(".eyes_5-js").hidden = true
+    document.querySelector(".eyes_3-js").hidden = false
+})
+
+document.querySelector(".eyes4-js").addEventListener('click', () => {
+    document.querySelector(".eyes_1-js").hidden = true
+    document.querySelector(".eyes_4-js").hidden = false
+})
+
+document.querySelector(".eyes4-js").addEventListener('click', () => {
+    document.querySelector(".eyes_2-js").hidden = true
+    document.querySelector(".eyes_4-js").hidden = false
+})
+
+document.querySelector(".eyes4-js").addEventListener('click', () => {
+    document.querySelector(".eyes_3-js").hidden = true
+    document.querySelector(".eyes_4-js").hidden = false
+})
+
+document.querySelector(".eyes4-js").addEventListener('click', () => {
+    document.querySelector(".eyes_5-js").hidden = true
+    document.querySelector(".eyes_4-js").hidden = false
+})
+
+document.querySelector(".eyes5-js").addEventListener('click', () => {
+    document.querySelector(".eyes_1-js").hidden = true
+    document.querySelector(".eyes_5-js").hidden = false
+})
+
+document.querySelector(".eyes5-js").addEventListener('click', () => {
+    document.querySelector(".eyes_2-js").hidden = true
+    document.querySelector(".eyes_5-js").hidden = false
+})
+
+document.querySelector(".eyes5-js").addEventListener('click', () => {
+    document.querySelector(".eyes_3-js").hidden = true
+    document.querySelector(".eyes_5-js").hidden = false
+})
+
+document.querySelector(".eyes5-js").addEventListener('click', () => {
+    document.querySelector(".eyes_4-js").hidden = true
+    document.querySelector(".eyes_5-js").hidden = false
+})
+
+document.querySelector(".clothes1-js").addEventListener('click', () => {
+    document.querySelector(".clothes_2-js").hidden = true
+    document.querySelector(".clothes_1-js").hidden = false
+})
+
+document.querySelector(".clothes1-js").addEventListener('click', () => {
+    document.querySelector(".clothes_3-js").hidden = true
+    document.querySelector(".clothes_1-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes1-js").addEventListener('click', () => {
+    document.querySelector(".clothes_4-js").hidden = true
+    document.querySelector(".clothes_1-js").hidden = false
+})
+
+document.querySelector(".clothes1-js").addEventListener('click', () => {
+    document.querySelector(".clothes_5-js").hidden = true
+    document.querySelector(".clothes_1-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes2-js").addEventListener('click', () => {
+    document.querySelector(".clothes_1-js").hidden = true
+    document.querySelector(".clothes_2-js").hidden = false
+})
+
+document.querySelector(".clothes2-js").addEventListener('click', () => {
+    document.querySelector(".clothes_3-js").hidden = true
+    document.querySelector(".clothes_2-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes2-js").addEventListener('click', () => {
+    document.querySelector(".clothes_4-js").hidden = true
+    document.querySelector(".clothes_2-js").hidden = false
+})
+
+document.querySelector(".clothes2-js").addEventListener('click', () => {
+    document.querySelector(".clothes_5-js").hidden = true
+    document.querySelector(".clothes_2-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes3-js").addEventListener('click', () => {
+    document.querySelector(".clothes_1-js").hidden = true
+    document.querySelector(".clothes_3-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
+
+document.querySelector(".clothes3-js").addEventListener('click', () => {
+    document.querySelector(".clothes_2-js").hidden = true
+    document.querySelector(".clothes_3-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
+
+document.querySelector(".clothes3-js").addEventListener('click', () => {
+    document.querySelector(".clothes_4-js").hidden = true
+    document.querySelector(".clothes_3-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
+
+document.querySelector(".clothes3-js").addEventListener('click', () => {
+    document.querySelector(".clothes_5-js").hidden = true
+    document.querySelector(".clothes_3-js").hidden = false
+})
+
+document.querySelector(".clothes4-js").addEventListener('click', () => {
+    document.querySelector(".clothes_1-js").hidden = true
+    document.querySelector(".clothes_4-js").hidden = false
+})
+
+document.querySelector(".clothes4-js").addEventListener('click', () => {
+    document.querySelector(".clothes_2-js").hidden = true
+    document.querySelector(".clothes_4-js").hidden = false
+})
+
+document.querySelector(".clothes4-js").addEventListener('click', () => {
+    document.querySelector(".clothes_3-js").hidden = true
+    document.querySelector(".clothes_4-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes4-js").addEventListener('click', () => {
+    document.querySelector(".clothes_5-js").hidden = true
+    document.querySelector(".clothes_4-js").hidden = false
+    document.querySelector(".pant-js").hidden = false
+})
+
+document.querySelector(".clothes5-js").addEventListener('click', () => {
+    document.querySelector(".clothes_1-js").hidden = true
+    document.querySelector(".clothes_5-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
+
+document.querySelector(".clothes5-js").addEventListener('click', () => {
+    document.querySelector(".clothes_2-js").hidden = true
+    document.querySelector(".clothes_5-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
+
+document.querySelector(".clothes5-js").addEventListener('click', () => {
+    document.querySelector(".clothes_3-js").hidden = true
+    document.querySelector(".clothes_5-js").hidden = false
+})
+
+document.querySelector(".clothes5-js").addEventListener('click', () => {
+    document.querySelector(".clothes_4-js").hidden = true
+    document.querySelector(".clothes_5-js").hidden = false
+    document.querySelector(".pant-js").hidden = true
+})
